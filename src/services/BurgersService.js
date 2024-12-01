f906e38c3e279be63d755b4d1f5cf453479167ab
Michael Giesler <michael.giesler1@icloud.com>
2024-11-30 18:49:42 -0700

    implemented burger service including core CRUD functions

---
+++ src/services/BurgersService.js
@@ -0,0 +1,25 @@
+import { dbContext } from '../db/DbContext'
+import { BadRequest } from '../utils/Errors'
+
+class BurgersService {
+  async getBurgers() {
+    const burgers = await dbContext.Burgers.find()
+    return burgers
+  }
+
+  async createBurger(burgerData) {
+    const burgers = await dbContext.Burgers.find()
+    return burgers
+  }
+
+  async deleteBurger(burgerId) {
+    const burger = await dbContext.Burgers.findById(burgerId)
+    if (!burger) {
+      throw new BadRequest('Invalid Burger Id')
+    }
+    await dbContext.Burgers.deleteOne({ _id: burgerId })
+    return `${burger.name} has been removed from the menu`
+  }
+}
+
+export const burgersService = new BurgersService()
