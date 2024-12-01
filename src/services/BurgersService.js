ccdddf004ab135d51eebc1fbabf6a2ecb1935a51
Michael Giesler <michael.giesler1@icloud.com>
2024-12-01 12:44:03 -0700

    added update burger functionality to burgers service

--- src/services/BurgersService.js
+++ src/services/BurgersService.js
@@ -20,6 +20,20 @@ class BurgersService {
     await dbContext.Burgers.deleteOne({ _id: burgerId })
     return `${burger.name} has been removed from the menu`
   }
+
+  async updateBurger(burgerId, updateData) {
+    const burger = await dbContext.Burgers.findById(burgerId)
+    if (!burger) {
+      throw new BadRequest('Invalid Burger Id')
+    }
+
+    if (updateData.name) burger.name = updateData.name
+    if (updateData.price) burger.price = updateData.price
+    if (updateData.description) burger.description = updateData.description
+
+    await burger.save()
+    return burger
+  }
 }
 
 export const burgersService = new BurgersService() 
